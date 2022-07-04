2c7cc03f2293195b2d21276912a72eb76f14b88a
Anna <stroeva_anna@ukr.net>
2022-07-04 19:31:10 +0300

    Додала новий проект Landing page Daisy

---
+++ js/app.js
@@ -0,0 +1,21 @@
+/* підключення фільтру */
+
+let mixer = mixitup ('.container-filter')
+
+/* scrollup button */
+
+$(function () {
+    $('.scrollup').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 1000);
+    })
+})
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 200) {
+        $('.scrollup').fadeIn();
+    } else {
+        $('.scrollup').fadeOut();
+    }
+});
